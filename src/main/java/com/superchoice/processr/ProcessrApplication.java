96014209537069f5d5b652de4ee54a79da9151d3
xuwei <email.xuwei@gmail.com>
2020-08-05 22:23:01 +1000

    first commit

---
+++ src/main/java/com/superchoice/processr/ProcessrApplication.java
@@ -0,0 +1,13 @@
+package com.superchoice.processr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessrApplication.class, args);
+	}
+
+}
