9290282ce378026e92aabb01f4cc758338a8a5bc
xuwei <email.xuwei@gmail.com>
2020-08-11 23:30:51 +1000

    updated logging config

--- src/main/java/com/superchoice/processr/exception/CustomExceptionHandler.java
+++ src/main/java/com/superchoice/processr/exception/CustomExceptionHandler.java
@@ -3,6 +3,8 @@ package com.superchoice.processr.exception;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,10 +14,13 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ControllerAdvice
 public class CustomExceptionHandler {
 	
+	private static final Logger logger = LoggerFactory.getLogger(CustomExceptionHandler.class);
+	
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public Map<String, String> handleValidationExceptions(
 	  MethodArgumentNotValidException ex) {
+		logger.error(ex.getLocalizedMessage());
 	    Map<String, String> errors = new HashMap<>();
 	    ex.getBindingResult().getAllErrors().forEach((error) -> {
 	        String fieldName = error.getCode();
